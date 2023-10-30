0da432daef11c88cf35f6912920e33b893636b26
SamarAlkhayyat <149406470+SamarAlkhayyat@users.noreply.github.com>
2023-10-30 20:50:15 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+	public static void main{
+		for(int i = 1; i <=5; i++){
+			for(int j = 1; j <=5; j++){
+				System.out.println(i*j+" ");
+			}
+			System.out.println("\n");
+		}
+}
